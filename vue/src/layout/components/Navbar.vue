0570417670097155e18fbea0d95af750515ffe8d
seyan <961595030@qq.com>
2021-11-21 14:40:49 +0800

    头像

--- vue/src/layout/components/Navbar.vue
+++ vue/src/layout/components/Navbar.vue
@@ -23,7 +23,7 @@
       </div>
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar" class="user-avatar">
+          <img :src="'/avatar.gif'" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
